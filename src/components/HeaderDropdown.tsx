f1b350cda44f64eea2fe62dd8258a4913967eb8a
jackdishman <me@jackdishman.com>
2022-10-24 13:58:55 +0200

    feat: small styling fixes

--- src/components/HeaderDropdown.tsx
+++ src/components/HeaderDropdown.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 const HeaderDropdown = () => {
 	return (
 		<div className="bg-black text-zinc-200 flex flex-col rounded-bl-xl shadow-lg">
-			<Link to="/" className="px-5 py-2 border-b border-zinc-200 border-l">Intro</Link>
-			<Link to="/dev" className="px-5 py-2 border-b border-zinc-200 border-l">Experience</Link>
-			<Link to="/education" className="rounded-bl-xl border-l border-b border-zinc-200 p-2">Education</Link>
+			<Link to="/" className="px-5 py-2 border-b border-zinc-200 border-l text-right">Introduction</Link>
+			<Link to="/dev" className="px-5 py-2 border-b border-zinc-200 border-l text-right">Experience</Link>
+			<Link to="/education" className="rounded-bl-xl border-l border-b border-zinc-200 px-5 py-2 text-right">Education</Link>
 		</div>
 	)
 }
--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -4,11 +4,10 @@ import TailwindIcon from "./icons/TailwindIcon";
 const Portfolio = () => {
 	return (
 		<div className="w-full sm:w-3/4 lg:w-1/2 border-b border-slate-900 mt-12 pb-10">
-			<p className="text-slate-900 text-sm">This website is made with React.js and deployed using GitHub Pages. Source code can be viewed <a href="https://github.com/jackdishman/portfolio-react" target="_blank" className="underline text-slate-500">here</a>.</p>
-			<div className="flex justify-center h-6">
+			<p className="text-slate-900 text-sm text-center">This website is made with React.js and deployed using GitHub Pages. Source code can be viewed <a href="https://github.com/jackdishman/portfolio-react" target="_blank" className="underline text-slate-500">here</a>.</p>
+			<div className="flex justify-center h-6 mt-3">
 				<ReactIcon />
 				<span className="px-2 text-slate-500">x</span>
-				
 				<TailwindIcon />
 			</div>
 		</div>

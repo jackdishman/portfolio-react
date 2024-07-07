3b7e1e7a5af7cf4b53cdcff5a5589e6aecbeef0f
jackdishman <me@jackdishman.com>
2024-07-07 16:01:24 -0400

    feat: add ts

--- src/components/Web3.tsx
+++ src/components/Web3.tsx
@@ -1,39 +1,41 @@
 const Web3 = () => {
-	return (
-		<div className="w-full sm:w-3/4 lg:w-1/2">
-			<div className="pt-5 mt-10 border-t border-gray-900">
-				<h2 className="text-3xl text-center">
-					Blockchain // Smart Contract Developer
-				</h2>
-				<div className="flex justify-between mt-5">
-					<span className="underline">Freelance</span>
-					<span className="italic">June 2021 - Present</span>
-				</div>
-				{/* Smart contract deployer */}
-				<p className="mt-5 text-gray-900 text-sm">
-					Built an Ethereum smart contract deployer that compiles Solidity code
-					into EVM-readable bytecode and then deploys to the Ethereum
-					blockchain. Technologies used include Solidity, ES6 JavaScript,
-					Web3.js, Truffle, and Infura.
-				</p>
-				<div className="text-center mt-2">
-					<a
-						href="https://etherscan.io/address/0x99e7f7C778555dDf0504f94efF7E268e8D6e0eA3"
-						target="_blank"
-						className="text-gray-500 text-sm">
-						View contract
-					</a>
-					<span className="px-2 text-gray-500"> | </span>
-					<a
-						href="https://github.com/jackdishman/solidity-smart-contract-deployer"
-						target="_blank"
-						className="text-gray-500 text-sm">
-						View source
-					</a>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full sm:w-3/4 lg:w-1/2">
+      <div className="pt-10 mt-12 border-t border-gray-900">
+        <h2 className="text-3xl text-center">
+          Blockchain // Smart Contract Developer
+        </h2>
+        <div className="flex justify-between mt-5">
+          <span className="underline">Freelance</span>
+          <span className="italic">June 2021 - Present</span>
+        </div>
+        {/* Smart contract deployer */}
+        <p className="mt-5 text-gray-900 text-sm">
+          Built an Ethereum smart contract deployer that compiles Solidity code
+          into EVM-readable bytecode and then deploys to the Ethereum
+          blockchain. Technologies used include Solidity, ES6 JavaScript,
+          Web3.js, Truffle, and Infura.
+        </p>
+        <div className="text-center mt-2">
+          <a
+            href="https://etherscan.io/address/0x99e7f7C778555dDf0504f94efF7E268e8D6e0eA3"
+            target="_blank"
+            className="text-gray-500 text-sm"
+          >
+            View contract
+          </a>
+          <span className="px-2 text-gray-500"> | </span>
+          <a
+            href="https://github.com/jackdishman/solidity-smart-contract-deployer"
+            target="_blank"
+            className="text-gray-500 text-sm"
+          >
+            View source
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Web3;
--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -7,47 +7,51 @@ import Portfolio from "../components/Portfolio";
 import Rootroop from "../components/Rootroop";
 import Sapient from "../components/Sapient";
 import Socials from "../components/Socials";
+import Tokensoft from "../components/Tokensoft";
 import Web3 from "../components/Web3";
 import Whatsgoodly from "../components/Whatsgoodly";
 
 export default function Work() {
-	return (
-		<div className="p-5 z-0 relative">
-			<div className="flex justify-center">
-				<Portfolio />
-			</div>
-			{/* <div className="flex justify-center">
-				<Captains />
-			</div> */}
-			<div className="flex justify-center">
-				<EthBoston />
-			</div>
-			<div className="flex justify-center">
-				<Authentic />
-			</div>
-			<div className="flex justify-center">
-				<Blogchain />
-			</div>
-			<div className="flex justify-center">
-				<Rootroop />
-			</div>
-			<div className="flex justify-center">
-				<Web3 />
-			</div>
-			<div className="flex justify-center">
-				<Extole />
-			</div>
-			<div className="flex justify-center">
-				<Sapient />
-			</div>
-			<div className="flex justify-center">
-				<Whatsgoodly />
-			</div>
-			<div className="flex justify-center">
-				<div className="w-full sm:w-3/4 lg:w-1/2 pt-5 mt-10 border-t border-gray-900">
-					<Socials />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="p-5 z-0 relative">
+      <div className="flex justify-center">
+        <Portfolio />
+      </div>
+      <div className="flex justify-center">
+        <Captains />
+      </div>
+      <div className="flex justify-center">
+        <Tokensoft />
+      </div>
+      <div className="flex justify-center">
+        <EthBoston />
+      </div>
+      <div className="flex justify-center">
+        <Authentic />
+      </div>
+      <div className="flex justify-center">
+        <Blogchain />
+      </div>
+      <div className="flex justify-center">
+        <Rootroop />
+      </div>
+      <div className="flex justify-center">
+        <Web3 />
+      </div>
+      <div className="flex justify-center">
+        <Extole />
+      </div>
+      <div className="flex justify-center">
+        <Sapient />
+      </div>
+      <div className="flex justify-center">
+        <Whatsgoodly />
+      </div>
+      <div className="flex justify-center">
+        <div className="w-full sm:w-3/4 lg:w-1/2 pt-5 mt-10 border-t border-gray-900">
+          <Socials />
+        </div>
+      </div>
+    </div>
+  );
 }
